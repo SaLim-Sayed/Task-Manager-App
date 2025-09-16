2c69803e5717284729e4fe7778f40ca4781b1221
Salem Sayed <salemsayed981@gmail.com>
2025-09-17 00:06:47 +0300

    upd: app icon and splash-screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { Text, View } from 'react-native';
+import { ImageBackground } from 'expo-image';
 
 
 export default function TabLayout() {
 
   return (
-    <View className='flex-1 items-center justify-center mt-40 bg-red-600'>
+    <ImageBackground source={require('@/assets/images/bg.png')} style={{ flex: 1 }}>
+    <View className='flex-1 items-center justify-center mt-40'>
       <Text className='text-2xl font-bold text-white'>Tab Layout</Text>
     </View>
+    </ImageBackground>
   );
 }
